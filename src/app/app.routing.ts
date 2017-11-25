1e3a775978f698214ba237d79bcf03b1778bd4c4
Javier Buitrago <jbuitrago2014@fau.edu>
2017-11-25 17:10:28 -0500

    app routing.ts completed, creating about and home path

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,2 +1,17 @@
 import {ModuleWithProviders} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+
+import { MyHomeComponent } from './my-home/my-home.component';
+import { MyAboutComponent } from './my-about/my-about.component';
+
+const appRoutes: Routes = [ {
+   path: '',
+   component: MyHomeComponent
+},
+{
+  path: 'about',
+  component: MyAboutComponent
+}
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
